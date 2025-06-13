4b9d8a58654b67593c26f08eb5d930f484ebbe3b
DodoMCanva <106896424+DodoMCanva@users.noreply.github.com>
2025-06-13 13:45:51 +0100

    mostrar imagen

--- app/src/main/java/garcia/fernando/popcornfactory/MovieDetail.kt
+++ app/src/main/java/garcia/fernando/popcornfactory/MovieDetail.kt
@@ -1,6 +1,9 @@
 package garcia.fernando.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,14 @@ class MovieDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-
+        val lblImg : ImageView = findViewById(R.id.imagendetalle)
+        val lblTitulo : TextView = findViewById(R.id.titulodetalle)
+        val lblDesc : TextView = findViewById(R.id.descripciondetalle)
+        var imagen : Int = intent.getIntExtra("image", R.drawable.popcorn)
+        lblImg.setImageResource(imagen)
+        var titulo : String = intent.getStringExtra("titulo").toString()
+        lblTitulo.setText(titulo)
+        var descripcion : String = intent.getStringExtra("sinopsis").toString()
+        lblDesc.setText(descripcion)
     }
 }
